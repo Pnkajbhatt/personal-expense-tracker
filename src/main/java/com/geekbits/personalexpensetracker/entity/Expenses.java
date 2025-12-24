559e93cf445b3a5046889117be98b048b609542e
PANKAJ BHATT <211pankajbhatt@gmail.com>
2025-12-24 13:34:37 +0530

    Schema done

---
+++ src/main/java/com/geekbits/personalexpensetracker/entity/Expenses.java
@@ -0,0 +1,54 @@
+package com.geekbits.personalexpensetracker.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "expenses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Expenses {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @NotNull(message = "amount is required")
+    @Column(name = "amount", nullable = false, precision = 10, scale = 2)
+    private Double amount;
+
+
+    @NotNull(message = "category is required")
+    @Column(name = "category", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @NotBlank(message = "description is required")
+    @Column(name = "description", nullable = false, length = 255)
+    private String description;
+
+    @NotNull(message = "date is required")
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    public enum Category {
+        food,
+        transport,
+        entertainment,
+        bills,
+        other
+    }
+
+}
---
+++ src/main/java/com/geekbits/personalexpensetracker/entity/User.java
@@ -0,0 +1,36 @@
+package com.geekbits.personalexpensetracker.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data 
+@NoArgsConstructor  
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "username is required")
+    @Size(min = 1, max = 100, message = "Field must be between 1 and 100 characters")
+    @Column(name = "username", nullable = false, length = 100)
+    private String username;
+
+    @NotBlank(message = "email is required")
+    @Column(name = "email", nullable = false, length = 30,unique = true)
+    private String email;
+
+    @NonNull
+    @Size(min = 8, max = 30, message = "password must be between 8 and 30 characters")
+    @Column(name = "password", nullable = false, length = 30)
+    private String password;
+
+    @Column(name = "created_date")
+    private java.time.LocalDate created_date;
+    
+}
